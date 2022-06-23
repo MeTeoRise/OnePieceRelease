6086e7a4f259c1ffe552fd52d6c1169a8acdff55
MeTeoRise <teoleo777@hotmail.com>
2022-06-23 19:30:42 +0300

    Setup get spoilers lambda handler

---
+++ lambda_handlers.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+from markdown import markdown
+from release_exporter import get_latest_spoiler
+
+
+def get_spoilers_handler():
+    """This is a handler to pass the latest spoilers to lambda invokares"""
+    latest_spoiler = get_latest_spoiler()
+
+    html = markdown(latest_spoiler)
+    text = ''.join(BeautifulSoup(html, features="lxml").findAll(text=True))
+
+    print(text)
