9ee5d1997066cc74a73e7a64869ec7c46f0ff687
MeTeoRise <teoleo777@hotmail.com>
2022-06-26 10:37:12 +0300

    Clean and apply black

--- lambda_handlers.py
+++ lambda_handlers.py
@@ -3,11 +3,11 @@ from markdown import markdown
 from release_exporter import get_latest_spoiler
 
 
-def get_spoilers_handler():
+def get_spoilers_handler(event, context):
     """This is a handler to pass the latest spoilers to lambda invokares"""
     latest_spoiler = get_latest_spoiler()
 
     html = markdown(latest_spoiler)
-    text = ''.join(BeautifulSoup(html, features="lxml").findAll(text=True))
+    text = "".join(BeautifulSoup(html, features="lxml").findAll(text=True))
 
     print(text)
